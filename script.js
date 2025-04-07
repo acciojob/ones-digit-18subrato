228242d4b593e50e1dccdcd7073ee0164e0965c1
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-07 12:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function onesDigit(n) {
+	n = String(n);
+	n = n.trim();
 	let size = n.length;
 	return n[size-1];
 }
 
 // Do not change the code below
 
-const n = prompt("Enter Number:")
+const n = prompt("Enter Number:");
 alert(onesDigit(n));
